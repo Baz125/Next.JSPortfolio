2829d7caa85d86fa0400417330395b7934dfa51c
Baz125 <103266154+Baz125@users.noreply.github.com>
2023-09-22 10:05:52 +0200

    fixing error with Link element

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -7,14 +7,15 @@ import { HiArrowDown } from "react-icons/hi";
 const HeroSection = () => {
   return (
         <section id="home">
-            <div className="flex flex-col text-center items-center jestify-center sm:py-32 md:flex-row md:space-x-4 md:text-left md:py-52">
+          <div className="flex flex-col text-center items-center jestify-center sm:py-32 md:flex-row md:space-x-4 md:text-left md:py-52">
               <div>
                   <Image className="rounded-full shadow-2xl" src="/profile_pic.jpg" alt="" width={300} height={300}/>
               </div>
               <div>
                     <h1 className="font-bold text-4xl mt-6 md:text-7xl md:mt-0">Barry McGuirk</h1>
                     <p className="text-lg mt-4 mb-6 md:text-2xl">Full-stack Web Developer, Pre-Sales Solutions Engineer, SaaS Sales Professional</p>
-                    <Link
+                      {/* @ts-expect-error Server Component */}          
+                      <Link
                       to="projects"
                       className="hover:cursor-pointer text-neutral-100 font-semibold px-6 py-3 bg-teal-600 rounded shadow hover:bg-teal-700"
                       activeClass="active"
